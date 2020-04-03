d56e78959b55084fcbc9b73ce1e5680f051e83e0
tigrisko98 <kostia.tigrisko@gmail.com>
2020-04-03 15:35:14 +0300

    refactored signin.php and added validate function

--- index.php
+++ index.php
@@ -13,10 +13,23 @@ session_start();
     <h1>Authorization</h1>
     <form action="/auto_sales/vendor/signin.php" method="post">
         <label>Email</label>
-        <input type="email" name="email" placeholder="Input your email"><br>
+        <input type="email" name="email" placeholder="Input your email">
+        <?php
+        $email_error = $_SESSION['email_error'] ?? false;
+        echo "<p class='message'>" . $email_error . "</p>";
+        ?>
         <label>Password</label>
-        <input type="password" name="password" placeholder="Input your password"><br>
+        <input type="password" name="password" placeholder="Input your password">
+        <?php
+        $password_error = $_SESSION['password_error'] ?? false;
+        echo "<p class='message'>" . $password_error . "</p>";
+        ?>
         <input class="button" type="submit" name="submit" value="Login">
+        <?php
+        $email_password_error = $_SESSION['email_password_error'] ?? false;
+        echo "<p class='message'>" . $email_password_error . "</p>";
+        unset($_SESSION['email_error'], $_SESSION['password_error'], $_SESSION['email_password_error']);
+        ?>
         <p>Don't have an account? - <a href="/auto_sales/register.php">Sign up!</a></p>
     </form>
 </div>
